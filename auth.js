a0c8823c48e7195165cafef42df2e2877c07099a
samshiiine <21f1003512@ds.study.iitm.ac.in>
2024-08-15 23:05:56 +0530

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const router = express.Router();
+
+// Register
+router.post('/register', async (req, res) => {
+    const { username, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashedPassword, role });
+    await newUser.save();
+    res.json({ message: 'User registered!' });
+});
+
+// Login
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET);
+    res.json({ token });
+});
+
+module.exports = router;
